431ab3a5dd92f65d7c37db24907c98fab27d4368
Paras Gurnani <parasgurnani3@gmail.com>
2021-03-30 16:00:43 +0530

    Adding form

--- Model/firstModel.py
+++ Model/firstModel.py
@@ -1,12 +1,26 @@
 from imageai.Detection import ObjectDetection
 import os
 
-exec_path = os.getcwd()
+def determineProbability(img):
+    exec_path = os.getcwd()
+    print(exec_path)
+    exec_path,end_path = os.path.split(str(exec_path))
+    print(exec_path,end_path)
+    exec_path = os.path.join(exec_path,'PoacherWeb','media',img)
+    print(exec_path)
+    model_path=os.getcwd()
+    print(model_path)
 
-detector = ObjectDetection()
-detector.setModelTypeAsRetinaNet()
-detector.setModelPath(os.path.join(exec_path , "resnet50_coco_best_v2.1.0.h5"))
-detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image=os.path.join(exec_path , "image1.jpg"), output_image_path=os.path.join(exec_path , "output1.jpg"))
-for eachObject in detections:
-    print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
+    detector = ObjectDetection()
+    detector.setModelTypeAsRetinaNet()
+    detector.setModelPath(os.path.join(os.getcwd().join('Model'), "resnet50_coco_best_v2.1.0.h5"))
+    detector.loadModel()
+    detections = detector.detectObjectsFromImage(input_image=str(exec_path), output_image_path=os.path.join(exec_path , "output1.jpg"))
+    person_probability = 0
+    for eachObject in detections:
+        print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
+        if(eachObject['name']=='person'):
+            if(eachObject['percentage_probability']>person_probability):
+                person_probability = eachObject['percentage_probability']
+    print(detections)
+    return person_probability
--- src/PoacherWeb/Processing/views.py
+++ src/PoacherWeb/Processing/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import PoacherImage
 from .forms import *
+from django.views.generic import TemplateView
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 def index(request):
@@ -11,4 +13,18 @@ def index(request):
 def Process(request):
     print(request.POST)
     return HttpResponse('In Process It')
+
+class Home(TemplateView):
+    template_name = 'Processing.html'
+
+def upload(request):
+    if(request.method=='POST'):
+        uploaded_file = request.FILES['image']
+        print(uploaded_file.name)
+        print(uploaded_file.size)
+        fs=FileSystemStorage()
+        name = fs.save(uploaded_file.name,uploaded_file)
+        url = fs.url(name)
+        print(url)
+    return render(request,'Processing.html')
 
